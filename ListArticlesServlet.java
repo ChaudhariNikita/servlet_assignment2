ef7705b364ea3717ab43e0a59ca367daa44e8488
ChaudhariNikita <128126131+ChaudhariNikita@users.noreply.github.com>
2024-12-02 18:20:46 +0530

    Update ListArticlesServlet.java

--- ListArticlesServlet.java
+++ ListArticlesServlet.java
@@ -11,9 +11,7 @@ import java.util.List;
 import example.dao.ArticleDaoImplementation;
 import example.main.Article;
 
-/**
- * Servlet implementation class ListArticlesServlet
- */
+
 @WebServlet("/ListArticlesServlet")
 public class ListArticlesServlet extends HttpServlet {
 	private ArticleDao articleDAO = new ArticleDaoImplementation();
@@ -22,13 +20,11 @@ public class ListArticlesServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            // Fetch all articles from the database
+           
             List<Article> articles = articleDAO.getAllArticles();
 
-            // Set the articles as a request attribute
             request.setAttribute("articles", articles);
 
-            // Forward the request to the JSP page for displaying articles
             request.getRequestDispatcher("listArticles.jsp").forward(request, response);
         } catch (Exception e) {
             e.printStackTrace();
